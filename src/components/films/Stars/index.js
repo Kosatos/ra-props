c08620bc308783d36115940ca7fb68577f9512a3
Константин Музыка <kosatkinms@mail.ru>
2022-10-20 00:42:56 +0300

    task stars done

---
+++ src/components/films/Stars/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Star from './Star';
+import styles from './index.module.css';
+
+export default function Stars({ count }) {
+  if (count < 1 || count > 5 || typeof count !== 'number') {
+    return null;
+  }
+
+  function renderStars() {
+    const stars = [];
+    let i = 0;
+    while (i < count) {
+      stars.push(
+        <li key={i}>
+          <Star />
+        </li>
+      );
+      i++;
+    }
+    return stars;
+  }
+
+  return <ul className={`${styles['card-body-stars']}`}>{renderStars()}</ul>;
+}
+
+Stars.defaultProps = {
+  count: 0,
+};
+Stars.propTypes = {
+  count: PropTypes.number,
+};
